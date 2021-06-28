0acc60d1a9670f226fb1cd0bd22cf34d52cec04a
Tommaso <donnarumma@libero.it>
2021-06-28 11:02:39 +0200

    Primo cambiamento

---
+++ js/footer.js
@@ -0,0 +1,13 @@
+
+//      
+
+let data = new Date() ;
+let anno = data.getFullYear() ;
+
+// console.log(data);
+// console.log(anno);
+
+let copyright =  " Copyright &copy" + anno + " INC. Tutti i diritti. " ;  
+
+
+copyright = document.querySelector(".copyright").innerHTML= copyright;
